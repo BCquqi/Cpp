0c49436cd5de97be1616f59a352c55743db321ed
ButterCookie <jackyhuangjunqi@icloud.com>
2026-04-11 19:41:28 +0800

    Delete Notes directory

--- Notes/tarjan缩点后恢复.cpp
+++
@@ -1,9 +0,0 @@
-for (int i = 1;i <= n;i++)
-    for (auto j : G[i]) {
-        int x = id[i],y = id[j];
-        long long hash = 1ll * x * N + y;
-        if (x != y && s.count(hash) == 0) {
-            in[y]++,out[x]++;
-            s.insert(hash);
-        }
-    }
--- Notes/浮点数二分.cpp
+++
@@ -1,9 +0,0 @@
-const double eps = 1e-5;
-double L = 0,R = 1,ans = 0;
-while (R - L >= eps) {
-    double mid = (L + R) / 2;
-    if (check(mid)) {
-        ans = mid;
-        L = mid;
-    } else R = mid;
-}
