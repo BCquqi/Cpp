2da5aed79a321a1bdd9fdd91b5e41efefc16804d
ButterCoookie <Jackyhuangjunqi@icloud.com>
2026-09-25 08:25:16 +0800

    -

--- 2026-8/贪心/Megalomania.cpp
+++ 2026-8/贪心/Megalomania.cpp
@@ -7,18 +7,18 @@ const int N = 2e5 + 5;
 struct Node {int a,b;} s[N];
 
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 1;i <= n;i++) cin >> s[i].a >> s[i].b;
-	sort(s + 1,s + n + 1,[](Node x,Node y) {return x.b < y.b;});
-	int t = 0;
-	for (int i = 1;i <= n;i++) {
-		t += s[i].a;
-		if (t > s[i].b) {
-			cout << "No" << endl;
-			return 0;
-		}
-	}
-	cout << "Yes" << endl;
-	return 0;
+    int n;
+    cin >> n;
+    for (int i = 1;i <= n;i++) cin >> s[i].a >> s[i].b;
+    sort(s + 1,s + n + 1,[](Node x,Node y) {return x.b < y.b;});
+    int t = 0;
+    for (int i = 1;i <= n;i++) {
+        t += s[i].a;
+        if (t > s[i].b) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
 }
